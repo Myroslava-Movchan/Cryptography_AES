d9a2a9fdf276733cc0e3b79865b459bcc645215c
LevkoBe <lbeniakh@kse.org.ua>
2025-03-04 17:10:45 +0200

    feat: eas cbc

---
+++ AES_CBC.cs
@@ -0,0 +1,26 @@
+﻿namespace Cryptography_AES
+{
+    public class AES_CBC : AESMode {
+        private byte[] iv;
+
+        public AES_CBC(byte[] key, byte[] iv) : base(key) {
+            if (iv.Length != BlockSize) throw new ArgumentException("Invalid IV size.");
+            this.iv = iv;
+        }
+
+        protected override byte[] EncryptBlock(byte[] block) {
+            for (int i = 0; i < BlockSize; i++) block[i] ^= iv[i];
+            byte[] encrypted = aes.Encrypt(block);
+            iv = encrypted;
+            return encrypted;
+        }
+
+        protected override byte[] DecryptBlock(byte[] block) {
+            byte[] decrypted = aes.Decrypt(block);
+            for (int i = 0; i < BlockSize; i++) decrypted[i] ^= iv[i];
+            iv = block;
+            return decrypted;
+        }
+    }
+
+}
